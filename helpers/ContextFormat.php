7a43f888a6af63955250c4d38d04461c40c47f7f
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-28 18:34:42 +0400

    helpers\ContextFormat: Normalization of context format

---
+++ helpers/ContextFormat.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package axy\creator
+ */
+
+namespace axy\creator\helpers;
+
+use axy\creator\errors\InvalidContext;
+
+/**
+ * Normalization of context format
+ *
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+class ContextFormat
+{
+    /**
+     * Normalizes of context format
+     *
+     * @param array $context
+     * @return array
+     * @throws \axy\creator\errors\InvalidContext
+     */
+    public static function normalize(array $context)
+    {
+        $diff = \array_diff_key($context, self::$defaults);
+        if (!empty($diff)) {
+            $errmsg = 'unknown index '.\implode(', ', \array_keys($diff));
+            throw new InvalidContext($errmsg);
+        }
+        if (!empty($context['namespace'])) {
+            if (\preg_match('/^\\\\?(.*?)\\\\?$/s', $context['namespace'], $ns)) {
+                $context['namespace'] = $ns[1].'\\';
+            }
+        } else {
+            $context['namespace'] = '\\';
+        }
+        return \array_replace(self::$defaults, $context);
+    }
+
+    /**
+     * @var array
+     */
+    private static $defaults = [
+        'namespace' => '',
+        'args' => [],
+        'append_args' => [],
+        'use_options' => false,
+        'classname' => null,
+        'creator' => null,
+        'parent' => null,
+        'validator' => null,
+    ];
+}
---
+++ tests/helpers/ContextFormatTest.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @package axy\creator
+ */
+
+namespace axy\creator\tests\helpers;
+
+use axy\creator\helpers\ContextFormat;
+
+/**
+ * @coversDefaultClass axy\creator\helpers\ContextFormat
+ */
+class ContextFormatTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::normalize
+     * @dataProvider providerNormalize
+     * @param mixed $pointer
+     * @param array $expected (null - exception)
+     */
+    public function testNormalize($context, $expected = null)
+    {
+        if (!$expected) {
+            $this->setExpectedException('axy\creator\errors\InvalidContext');
+        }
+        $actual = ContextFormat::normalize($context);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function providerNormalize()
+    {
+        return [
+            [
+                [],
+                [
+                    'namespace' => '\\',
+                    'args' => [],
+                    'append_args' => [],
+                    'use_options' => false,
+                    'classname' => null,
+                    'creator' => null,
+                    'parent' => null,
+                    'validator' => null,
+                ],
+            ],
+            [
+                [
+                    'namespace' => 'my\ns',
+                    'args' => [1, 2],
+                    'use_options' => true,
+                    'classname' => null,
+                ],
+                [
+                    'namespace' => 'my\ns\\',
+                    'args' => [1, 2],
+                    'append_args' => [],
+                    'use_options' => true,
+                    'classname' => null,
+                    'creator' => null,
+                    'parent' => null,
+                    'validator' => null,
+                ],
+            ],
+            [
+                [
+                    'namespace' => '\my\ns\\',
+                    'args' => [1, 2],
+                    'use_options' => true,
+                    'classname' => null,
+                ],
+                [
+                    'namespace' => 'my\ns\\',
+                    'args' => [1, 2],
+                    'append_args' => [],
+                    'use_options' => true,
+                    'classname' => null,
+                    'creator' => null,
+                    'parent' => null,
+                    'validator' => null,
+                ],
+            ],
+            [
+                [
+                    'namespace' => '\\',
+                    'args' => [1, 2],
+                    'use_options' => false,
+                ],
+                [
+                    'namespace' => '\\',
+                    'args' => [1, 2],
+                    'append_args' => [],
+                    'use_options' => false,
+                    'classname' => null,
+                    'creator' => null,
+                    'parent' => null,
+                    'validator' => null,
+                ],
+            ],
+            [
+                [
+                    'namespace' => 'my\ns',
+                    'args' => [1, 2],
+                    'x' => 1,
+                    'y' => 2,
+                ],
+                null,
+            ],
+        ];
+    }
+}
