1dd5406133a374c88db42a9cc1b32dceed6e8ae2
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-28 18:19:12 +0400

    helpers\Validator: Validate result of creating

---
+++ helpers/Validator.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package axy\creator
+ */
+
+namespace axy\creator\helpers;
+
+use axy\creator\errors\InvalidPointer;
+use axy\callbacks\Callback;
+
+/**
+ * Validate result of creating
+ *
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+class Validator
+{
+    /**
+     * Check result
+     *
+     * @param mixed $result
+     *        the result of creating
+     * @param array $context
+     *        the context of the creator
+     * @throws \axy\creator\errors\InvalidPointer
+     */
+    public static function validate($result, array $context)
+    {
+        if (!empty($context['parent'])) {
+            if (!($result instanceof $context['parent'])) {
+                throw new InvalidPointer('Object must be an instance of '.$context['parent']);
+            }
+        }
+        if (!empty($context['validator'])) {
+            if (!Callback::call($context['validator'], [$result])) {
+                throw new InvalidPointer('Object is not valid');
+            }
+        }
+        return true;
+    }
+}
---
+++ tests/helpers/ValidatorTest.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * @package axy\creator
+ */
+
+namespace axy\creator\tests\helpers;
+
+use axy\creator\helpers\Validator;
+use axy\creator\tests\nstst\Target;
+
+/**
+ * @coversDefaultClass axy\creator\helpers\Validator
+ */
+class ValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::validate
+     * @dataProvider providerValidate
+     * @param mixed $result
+     * @param array $context
+     * @param boolean $valid
+     */
+    public function testValidate($result, $context, $valid)
+    {
+        if (!$valid) {
+            $this->setExpectedException('axy\creator\errors\InvalidPointer');
+        }
+        Validator::validate($result, $context);
+    }
+
+    /**
+     * @return array
+     */
+    public function providerValidate()
+    {
+        return [
+            [
+                $this,
+                [],
+                true,
+            ],
+            [
+                $this,
+                [
+                    'parent' => 'PHPUnit_Framework_TestCase',
+                ],
+                true,
+            ],
+            [
+                new Target(),
+                [
+                    'parent' => 'PHPUnit_Framework_TestCase',
+                ],
+                false,
+            ],
+            [
+                new Target(1, 2, 3),
+                [
+                    'validator' => function ($instance) {
+                        return (\array_sum($instance->args) === 6);
+                    },
+                ],
+                true,
+            ],
+            [
+                new Target(1, 2),
+                [
+                    'validator' => function ($instance) {
+                        return (\array_sum($instance->args) === 6);
+                    },
+                ],
+                false,
+            ],
+            [
+                new Target(1, 2, 3),
+                [
+                    'parent' => 'axy\creator\tests\nstst\Target',
+                    'validator' => function ($instance) {
+                        return (\array_sum($instance->args) === 6);
+                    },
+                ],
+                true,
+            ],
+            [
+                new Target(1, 2),
+                [
+                    'parent' => 'axy\creator\tests\nstst\Target',
+                    'validator' => function ($instance) {
+                        return (\array_sum($instance->args) === 6);
+                    },
+                ],
+                false,
+            ],
+            [
+                12,
+                [
+                    'parent' => 'axy\creator\tests\nstst\Target',
+                ],
+                false,
+            ],
+        ];
+    }
+}
