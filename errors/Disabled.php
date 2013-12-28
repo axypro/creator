a671b34b0e4260312aff575aa388145a4db5ce21
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-28 16:35:42 +0400

    errors/Disabled: Subservice is disabled

---
+++ errors/Disabled.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package axy\creator
+ */
+
+namespace axy\creator\errors;
+
+/**
+ * Subservice is disabled
+ *
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+class Disabled extends \axy\errors\Disabled
+{
+}
